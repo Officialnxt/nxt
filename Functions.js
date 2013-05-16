03db7732b4fc13160ebee74cfb2f7d16dfd3c27d
Officialnxt <esmith233@textfree.us>
2013-05-16 15:03:35 -0700

    Created Functions.js

--- Functions.js
+++ Functions.js
@@ -1,3 +1,42 @@
-## A New Post
+//Scroll to the top
 
-Enter text in [Markdown](http://daringfireball.net/projects/markdown/). Use the toolbar above, or click the **?** button for formatting help.
+var timeOut;
+function scrollToTop() {
+  if (document.body.scrollTop!=0 || document.documentElement.scrollTop!=0){
+    window.scrollBy(0,-50);
+    timeOut=setTimeout('scrollToTop()',10);
+    document.getElementById("search").focus();
+  }
+  else clearTimeout(timeOut);
+}
+
+// Toggle(Index.php)
+
+$(document).ready(function() {
+  $('#link1').click(function(){ 
+       $('#world').slideToggle('fast'); 
+        return false; 
+      });
+
+  $('#link2').click(function(){ 
+       $('#sports').slideToggle('fast'); 
+        return false; 
+      });
+
+  $('#link3').click(function(){ 
+       $('#tech').slideToggle('fast'); 
+        return false; 
+      });
+
+  $('#link4').click(function(){ 
+       $('#gamming').slideToggle('fast'); 
+        return false; 
+      });
+
+  $('#link5').click(function(){ 
+       $('#other').slideToggle('fast'); 
+        return false; 
+      });
+
+
+}); 
