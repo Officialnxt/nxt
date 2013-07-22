56c19e4bccc9a6c95a48a1a189078eacde19fedd
Officialnxt <esmith233@textfree.us>
2013-07-22 18:59:48 -0400

    Update Index.php

--- Index.php
+++ Index.php
@@ -6,19 +6,22 @@
 <body>
 		<a id='link1' href='#'><h2>World News</h2></a>
 		<div id='world'>
-				<a href='http://nxt.comxa.com/discover'><h1>Discover</h1></a><hr />
 
 		<a id='link1' title='hide/show' href='#'><h2>World News</h2></a>
 		<div id='world'>
 		<?php
+
 		$html = "";
 		$url = "http://feeds.bbci.co.uk/news/rss.xml";
-		$xml = simplexml_load_file($url);
+		$curl = curl_init($url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		$data = curl_exec($curl);
+		$xml = simplexml_load_string($data);
 		for($i = 0; $i < 1; $i++){
-	
-			$title = $xml->channel->item[$i]->title;
+		
+			$title = utf8_decode($xml->channel->item[$i]->title);
 			$link = $xml->channel->item[$i]->link;
-			$description = $xml->channel->item[$i]->description;
+			$description = utf8_decode($xml->channel->item[$i]->description);
 			$pubDate = $xml->channel->item[$i]->pubDate;
 	
 			$html .= "<a target='_blank' href='$link'>$title</a><br />";
@@ -31,16 +34,19 @@
 
 		
 		<a id='link2' title='hide/show' href='#'><h2>Sports</h2></a>
-		<div id='sports'>
+		<div id='sport'>
 		<?php
 		$html = "";
-		$url = "http://news.yahoo.com/rss/sports";
-		$xml = simplexml_load_file($url);
+	$url = "http://sports.espn.go.com/espn/rss/news";
+		$curl = curl_init($url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		$data = curl_exec($curl);
+		$xml = simplexml_load_string($data);
 		for($i = 0; $i < 1; $i++){
 	
-			$title = $xml->channel->item[$i]->title;
+			$title = utf8_decode($xml->channel->item[$i]->title);
 			$link = $xml->channel->item[$i]->link;
-			$description = $xml->channel->item[$i]->description;
+			$description = utf8_decode($xml->channel->item[$i]->description);
 			$pubDate = $xml->channel->item[$i]->pubDate;
 	
 			$html .= "<a target='_blank'  href='$link'>$title</a><br />";
@@ -55,13 +61,16 @@
 		<div id='tech'>
 		<?php
 		$html = "";
-		$url = "http://feeds.feedburner.com/TechCrunch/";
-		$xml = simplexml_load_file($url);
+		$url = "http://feeds.arstechnica.com/arstechnica/technology-lab";
+		$curl = curl_init($url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		$data = curl_exec($curl);
+		$xml = simplexml_load_string($data);
 		for($i = 0; $i < 1; $i++){
 	
-			$title = $xml->channel->item[$i]->title;
+			$title = utf8_decode($xml->channel->item[$i]->title);
 			$link = $xml->channel->item[$i]->link;
-			$description = $xml->channel->item[$i]->description;
+			$description = utf8_decode($xml->channel->item[$i]->description);
 			$pubDate = $xml->channel->item[$i]->pubDate;
 	
 			$html .= "<a target='_blank'  href='$link'>$title</a><br />";
@@ -72,17 +81,168 @@
 		?>		
 		</div>
 
-		<a id='link4' title='hide/show' href='#'><h2>Gamming</h2></a>
-		<div id='gamming'>
+		<a id='link4' title='hide/show' href='#'><h2>Other</h2></a>
+		<div id='other'>
 		<?php
 		$html = "";
-		$url = "http://news.yahoo.com/rss/gaming";
-		$xml = simplexml_load_file($url);
+		$url = "http://digg.com/rss/top.rss";
+		$curl = curl_init($url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		$data = curl_exec($curl);
+		$xml = simplexml_load_string($data);
 		for($i = 0; $i < 1; $i++){
 	
-			$title = $xml->channel->item[$i]->title;
+			$title = utf8_decode($xml->channel->item[$i]->title);
 			$link = $xml->channel->item[$i]->link;
-			$description = $xml->channel->item[$i]->description;
+			$description = utf8_decode($xml->channel->item[$i]->description);
+			$pubDate = $xml->channel->item[$i]->pubDate;
+	
+			$html .= "<a target='_blank'  href='$link'>$title</a><br />";
+			$html .= "$description";
+			$html .= "<br />$pubDate<hr />";
+		}
+		echo $html;
+		?>
+		</div>
+
+		<a id='link5' title='hide/show' href='#'><h2>Satire</h2></a>
+		<div id='satire'>
+		<?php
+		$html = "";
+		$url = "http://feeds.feedburner.com/theonion/RWNp";
+		$curl = curl_init($url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		$data = curl_exec($curl);
+		$xml = simplexml_load_string($data);
+		for($i = 0; $i < 1; $i++){
+	
+		$title = utf8_decode($xml->channel->item[$i]->title);
+		$link = $xml->channel->item[$i]->link;
+		$description = utf8_decode($xml->channel->item[$i]->description);
+		$pubDate = $xml->channel->item[$i]->pubDate;
+	
+		$html .= "<a target='_blank'  href='$link'>$title</a><br />";
+		$html .= "$description";
+		$html .= "<br />$pubDate<hr />";
+		} 
+		echo $html;
+		?>
+		</div>
+      		</td>
+		<td>
+
+		<?php
+
+		$html = "";
+		$url = "http://news.yahoo.com/rss/";
+		$curl = curl_init($url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		$data = curl_exec($curl);
+		$xml = simplexml_load_string($data);
+		for($i = 0; $i < 1; $i++){
+	
+			$title = utf8_decode($xml->channel->item[$i]->title);
+			$link = $xml->channel->item[$i]->link;
+			$description = utf8_decode($xml->channel->item[$i]->description);
+			$pubDate = $xml->channel->item[$i]->pubDate;
+	
+			$html .= "<a target='_blank'  href='$link'>$title</a><br />";
+			$html .= "$description";
+			$html .= "<br />$pubDate<hr />";
+		}
+		echo $html;
+
+		?>
+
+		<b>Stocks</b><hr />
+
+		<?php
+
+		$html = "";
+		$url = "http://rss.cnn.com/rss/money_markets.rss";
+		$curl = curl_init($url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		$data = curl_exec($curl);
+		$xml = simplexml_load_string($data);
+		for($i = 0; $i < 1; $i++){
+	
+			$title = utf8_decode($xml->channel->item[$i]->title);
+			$link = $xml->channel->item[$i]->link;
+			$description = utf8_decode($xml->channel->item[$i]->description);
+			$pubDate = $xml->channel->item[$i]->pubDate;
+	
+			$html .= "<a target='_blank'  href='$link'>$title</a><br />";
+			$html .= "$description";
+			$html .= "<br />$pubDate<hr />";
+		}
+		echo $html;
+
+		?>
+
+		<b>Culture</b><hr />
+
+		<?php
+
+		$html = "";
+		$url = "http://www.bbc.com/culture/feed.rss";
+		$curl = curl_init($url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		$data = curl_exec($curl);
+		$xml = simplexml_load_string($data);
+		for($i = 0; $i < 1; $i++){
+	
+			$title = utf8_decode($xml->channel->item[$i]->title);
+			$link = $xml->channel->item[$i]->link;
+			$description = utf8_decode($xml->channel->item[$i]->description);
+			$pubDate = $xml->channel->item[$i]->pubDate;
+	
+			$html .= "<a target='_blank'  href='$link'>$title</a><br />";
+			$html .= "$description";
+			$html .= "<br />$pubDate<hr />";
+		}
+		echo $html;
+
+		?>
+
+		<b>Gamming</b><hr />
+
+		<?php
+
+		$html = "";
+		$url = "http://n4g.com/rss/news?channel=&sort=latest";
+		$curl = curl_init($url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		$data = curl_exec($curl);
+		$xml = simplexml_load_string($data);
+		for($i = 0; $i < 1; $i++){
+	
+			$title = utf8_decode($xml->channel->item[$i]->title);
+			$link = $xml->channel->item[$i]->link;
+			$description = utf8_decode($xml->channel->item[$i]->description);
+			$pubDate = $xml->channel->item[$i]->pubDate;
+	
+			$html .= "<a target='_blank'  href='$link'>$title</a><br />";
+			$html .= "$description";
+			$html .= "<br />$pubDate<hr />";
+		}
+		echo $html;
+
+		?>
+
+		<b>Music</b><hr />
+
+		<?php
+		$html = "";
+		$url = "http://www.rollingstone.com/siteServices/rss/musicNewsAndFeature";
+		$curl = curl_init($url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		$data = curl_exec($curl);
+		$xml = simplexml_load_string($data);
+		for($i = 0; $i < 1; $i++){
+	
+			$title = utf8_decode($xml->channel->item[$i]->title);
+			$link = $xml->channel->item[$i]->link;
+			$description = utf8_decode($xml->channel->item[$i]->description);
 			$pubDate = $xml->channel->item[$i]->pubDate;
 	
 			$html .= "<a target='_blank'  href='$link'>$title</a><br />";
@@ -90,87 +250,8 @@
 			$html .= "<br />$pubDate<hr />";
 		} 
 		echo $html;
-		?>		
-		</div>
-
-		<a id='link5' title='hide/show' href='#'><h2>Other</h2></a>
-		<div id='other'>
-		<?php
-		$html = "";
-		$url = "http://digg.com/rss/top.rss";
-		$xml = simplexml_load_file($url);
-		for($i = 0; $i < 1; $i++){
-	
-			$title = $xml->channel->item[$i]->title;
-			$link = $xml->channel->item[$i]->link;
-			$description = $xml->channel->item[$i]->description;
-			$pubDate = $xml->channel->item[$i]->pubDate;
-	
-			$html .= "<a target='_blank'  href='$link'>$title</a><br />";
-			$html .= "$description";
-			$html .= "<br />$pubDate<hr />";
-		}
-		echo $html;
-		?>		
-		</div>
-	
-		
-
-      		</td>
-		<td>
-		<?php
-
-		$html = "";
-		$url = "http://rss.cnn.com/rss/money_markets.rss";
-		$xml = simplexml_load_file($url);
-		for($i = 0; $i < 1; $i++){
-	
-			$title = $xml->channel->item[$i]->title;
-			$link = $xml->channel->item[$i]->link;
-			$description = $xml->channel->item[$i]->description;
-			$pubDate = $xml->channel->item[$i]->pubDate;
-	
-			$html .= "<a target='_blank'  href='$link'>$title</a><br />";
-			$html .= "$description";
-			$html .= "<br />$pubDate<hr />";
-		}
-		echo $html;
-
 		?>
 
-		<b>Bitcoin Exchange Rates</b><hr />
-
-		<?php
-
-		$url = 'https://blockchain.info/ticker';
-		$content = file_get_contents($url);
-		$json = json_decode($content, true);
-
-		$usd = $json['USD']['last'];
-		$cny = $json['CNY']['last'];
-		$jpy = $json['JPY']['last'];
-		$sgd = $json['SGD']['last'];
-		$hkd = $json['HKD']['last'];
-		$cad = $json['CAD']['last'];
-		$aud = $json['AUD']['last'];
-		$nzd = $json['NXD']['last'];
-		$eur = $json['EUR']['last'];
-		$rub = $json['RUB']['last'];
-
-		echo "<b>USD:</b> $usd<br />";
-		echo "<b>CNY:</b> $cny<br />";
-		echo "<b>JPY:</b> $jpy<br />";
-		echo "<b>SGD:</b> $sgd<br />";
-		echo "<b>HKD:</b> $hkd<br />";
-		echo "<b>CAD:</b> $cad<br />";
-		echo "<b>AUD:</b> $aud<br />";
-		echo "<b>NZD:</b> $nzd<br />";
-		echo "<b>EUR:</b> $eur<br />";
-		echo "<b>RUB:</b> $rub<hr />";						
-
-		?>
-	
-		</div>
       	</td>
 
 
